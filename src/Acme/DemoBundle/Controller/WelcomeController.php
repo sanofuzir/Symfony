8a480da505ed23ae673950cdb77b4df934df94fb
sanofuzir <sano.fuzir@gmail.com>
2013-01-22 15:54:24 +0100

    third commit

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -3,6 +3,9 @@
 namespace Acme\DemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 
 class WelcomeController extends Controller
 {
@@ -14,5 +17,41 @@ class WelcomeController extends Controller
          *
          */
         return $this->render('AcmeDemoBundle:Welcome:index.html.twig');
+    }
+    
+    /**
+     * @Route("/o_meni", name="_o_meni")
+     * @Template()
+     */
+    public function o_meniAction()
+    {
+        return $this->render('AcmeDemoBundle:Welcome:o_meni.html.twig');
+    }
+    
+    /**
+     * @Route("/reference", name="_reference")
+     * @Template()
+     */
+    public function referenceAction()
+    {
+        return $this->render('AcmeDemoBundle:Welcome:reference.html.twig');
+    }
+    
+    /**
+     * @Route("/blog", name="_blog")
+     * @Template()
+     */
+    public function blogAction()
+    {
+        return $this->render('AcmeDemoBundle:Welcome:blog.html.twig');
+    }
+    
+    /**
+     * @Route("/kontakt", name="_kontakt")
+     * @Template()
+     */
+    public function kontaktAction()
+    {
+        return $this->render('AcmeDemoBundle:Welcome:kontakt.html.twig');
     }
 }
